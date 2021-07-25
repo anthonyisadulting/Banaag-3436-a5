08e48b42dc581b1e75679566004f9bef065e46c2
anthonyisadulting <anthonyisadulting@gmail.com>
2021-07-25 16:29:28 -0400

    fifth commit: delete button works

--- src/main/java/ucf/assignments/InventoryManagerController.java
+++ src/main/java/ucf/assignments/InventoryManagerController.java
@@ -1,8 +1,10 @@
 package ucf.assignments;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -63,11 +65,15 @@ public class InventoryManagerController implements Initializable {
 
     @FXML
     public void removeitem(ActionEvent actionEvent) {
+        //remove selected rows
+        //remember observablelist for item sorting
+        ObservableList<ItemInfo> selectedRows, allItems;
+        allItems = ItemTable.getItems();
+
+        selectedRows = ItemTable.getSelectionModel().getSelectedItems();
+        allItems.removeAll(selectedRows);
     }
 
-    @FXML
-    public void edititem(ActionEvent actionEvent) {
-    }
 
     @FXML
     public void sortbyvalue(ActionEvent actionEvent) {
@@ -82,6 +88,14 @@ public class InventoryManagerController implements Initializable {
     }
 
     @FXML
+    public void searchSerialNumber(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void searchName(ActionEvent actionEvent) {
+    }
+
+    @FXML
     public void loadaninventory(ActionEvent actionEvent) {
     }
 
@@ -91,17 +105,27 @@ public class InventoryManagerController implements Initializable {
 
 
 
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb){
     valueColumn.setCellValueFactory(new PropertyValueFactory<ItemInfo, String>("value"));
     serialColumn.setCellValueFactory(new PropertyValueFactory<ItemInfo, String>("serialnumber"));
     nameColumn.setCellValueFactory(new PropertyValueFactory<ItemInfo, String>("name"));
 
+
+    //edit initialization
     ItemTable.setEditable(true);
     valueColumn.setCellFactory(TextFieldTableCell.forTableColumn());
     serialColumn.setCellFactory(TextFieldTableCell.forTableColumn());
     nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 
+    //remove initialization
+    ItemTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
     }
+
+
 
 }
